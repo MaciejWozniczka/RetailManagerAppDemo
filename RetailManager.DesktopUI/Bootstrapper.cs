0114c816c06cfc3b10971abe1625672d9ed2eeb4
MaciejWozniczka <maciej.wozniczka@tpa-group.pl>
2022-08-11 22:33:09 +0200

    Add WPF project WPF with MVVM Project Setup

---
+++ RetailManager.DesktopUI/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using RetailManager.DesktopUI.ViewModels;
+using System.Windows;
+
+namespace RetailManager.DesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
