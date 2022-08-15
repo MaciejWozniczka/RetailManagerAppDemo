be37d57233320ac598e08232434ed64b3470ec46
MaciejWozniczka <maciej.wozniczka@tpa-group.pl>
2022-08-15 20:11:35 +0200

    Fixing namespace for DataManager

--- RetailManager.Api/Areas/HelpPage/App_Start/HelpPageConfig.cs
+++ RetailManager.Api/Areas/HelpPage/App_Start/HelpPageConfig.cs
@@ -26,7 +26,7 @@ namespace RetailManager.Api.Areas.HelpPage
     public static class HelpPageConfig
     {
         [SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters",
-            MessageId = "DataManager.Api.Areas.HelpPage.TextSample.#ctor(System.String)",
+            MessageId = "RetailManager.Api.Areas.HelpPage.TextSample.#ctor(System.String)",
             Justification = "End users may choose to merge this string with existing localized resources.")]
         [SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly",
             MessageId = "bsonspec",
--- RetailManager.Api/Properties/AssemblyInfo.cs
+++ RetailManager.Api/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataManager.Api")]
+[assembly: AssemblyTitle("RetailManager.Api")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataManager.Api")]
+[assembly: AssemblyProduct("RetailManager.Api")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
