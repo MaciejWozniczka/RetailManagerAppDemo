461f43fa2d10774a46679c144d80b93eaa774486
MaciejWozniczka <maciej.wozniczka@tpa-group.pl>
2022-08-15 20:05:57 +0200

    Getting User Data using Dapper Getting User Data

---
+++ DataManager/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using RetailManager.Library.DataAccess;
+using RetailManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace RetailManager.Api.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Account")]
+    public class UserController : ApiController
+    {
+        public List<User> GetById()
+        {
+            UserData data = new UserData();
+
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
---
+++ RetailManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,19 @@
+﻿using RetailManager.Library.Internal.DataAccess;
+using RetailManager.Library.Models;
+using System.Collections.Generic;
+
+namespace RetailManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<User> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+            var parameters = new { Id = Id };
+
+            var output = sql.LoadData<User, dynamic>("dbo.spUserLookup", parameters, "RetailManager");
+
+            return output;
+        }
+    }
+}
